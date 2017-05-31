9be98bac1063f4121f0431ac0ec62b402d7505e8
Joaquín Silveira <joaco.silveira@gmail.com>
2017-05-31 19:38:59 -0300

    En la segunda versión del controlador puedo darle instrucciones con índices numéricos preestablecidos al robot para que se mueva en distintas direcciones (adelante, atrás, derecha, detenerse, etc). Es un poco rústico aún porque no le he pedido que reporte de ninguna manera (como por ejemplo cuando tiene un objeto delante).

--- primerEjemploRemoteAPI/primerEjemploRemoteAPI/Source.cpp
+++ primerEjemploRemoteAPI/primerEjemploRemoteAPI/Source.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <iostream>
 using namespace std;
+#define FORWARD_MOVEMENT 1
+#define BACKWARD_MOVEMENT 2
+#define FORWARD_LEFT_MOVEMENT 3
+#define FORWARD_RIGHT_MOVEMENT 4
+#define BACKWARD_LEFT_MOVEMENT 5
+#define BACKWARD_RIGHT_MOVEMENT 6
+#define STOP 7
+
 extern "C" {
      #include "extApi.h"
    /*#include "extApiCustom.h" if you wanna use custom remote API functions! */
@@ -25,14 +33,37 @@ int main(int argc, char** argv)
 		simxGetObjectHandle(clientID,"bubbleRob_leftMotor",&leftMotorHandle,simx_opmode_blocking);
 		simxGetObjectHandle(clientID,"bubbleRob_rightMotor",&rightMotorHandle,simx_opmode_blocking);
 		while (respuesta){
-			if (respuesta == 1){
-				leftMotorSpeed = 4;
-				rightMotorSpeed = 1;
+			switch (respuesta){
+				case FORWARD_MOVEMENT:
+					leftMotorSpeed = 4;
+					rightMotorSpeed = 4;
+					break;
+				case BACKWARD_MOVEMENT:
+					leftMotorSpeed = -4;
+					rightMotorSpeed = -4;
+					break;
+				case FORWARD_RIGHT_MOVEMENT:
+					leftMotorSpeed = 4;
+					rightMotorSpeed = 1;
+					break;
+				case FORWARD_LEFT_MOVEMENT:
+					leftMotorSpeed = 1;
+					rightMotorSpeed = 4;
+					break;
+				case BACKWARD_RIGHT_MOVEMENT:
+					leftMotorSpeed = -4;
+					rightMotorSpeed = -1;
+					break;
+				case BACKWARD_LEFT_MOVEMENT:
+					leftMotorSpeed = -1;
+					rightMotorSpeed = -4;
+					break;
+				default:
+					leftMotorSpeed = 0;
+					rightMotorSpeed = 0;
+					break;
 			}
-			else {
-				leftMotorSpeed = 1;
-				rightMotorSpeed = 4;
-			}
+
 			simxSetJointTargetVelocity(clientID,leftMotorHandle,leftMotorSpeed,simx_opmode_oneshot);
 			simxSetJointTargetVelocity(clientID,rightMotorHandle,rightMotorSpeed,simx_opmode_oneshot);
 			cin>> respuesta;
